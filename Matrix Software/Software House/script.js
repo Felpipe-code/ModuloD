6c7dfffed689bbdd588c72fff3ea6fc3fc623047
Felipe Machado Leandro <fmltdai@gmail.com>
2026-04-17 09:19:50 -0300

    Add files via upload

---
+++ Matrix Software/Software House/script.js
@@ -0,0 +1,5 @@
+let botao = document.getElementById('menu-btn');
+let mobilemenu = document.getElementById('menu');
+botao.addEventListener("click",()=>{
+    mobilemenu.classList.toggle("hidden");
+}) 
